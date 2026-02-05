37779efa470509b34a598273452869ae6be678f4
Philipp Seifert-Kehrer <philipp.seifert-kehrer@posedio.com>
2026-02-06 00:25:06 +0100

    initial commit

---
+++ src/main/java/com/originalflipster/wait/Submission.java
@@ -0,0 +1,73 @@
+package com.originalflipster.wait;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+final class Submission {
+  private final String name;
+  private final LocalDateTime submission;
+  private final LocalDateTime deadline;
+  private final Duration maxDuration;
+  private final String result;
+
+  Submission(String name, LocalDateTime submission, LocalDateTime deadline, Duration maxDuration, String result) {
+    this.name = name;
+    this.submission = submission;
+    this.deadline = deadline;
+    this.maxDuration = maxDuration;
+    this.result = result;
+  }
+
+  String getName() {
+    return name;
+  }
+
+  long days() {
+    return submission.until(LocalDateTime.now(), ChronoUnit.DAYS);
+  }
+
+  long hours() {
+    return submission.until(LocalDateTime.now(), ChronoUnit.HOURS);
+  }
+
+  long secs() {
+    return submission.until(LocalDateTime.now(), ChronoUnit.SECONDS);
+  }
+
+  BigDecimal progressDone() {
+    if(deadline != null) {
+      return BigDecimal.valueOf(((double) secs()) / ((double) maxDuration.toSeconds() + submission.until(deadline, ChronoUnit.SECONDS))).multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP);
+    }
+    return BigDecimal.valueOf(((double) secs()) / ((double) maxDuration.toSeconds())).multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP);
+  }
+
+  private LocalDateTime start() {
+    if(deadline != null) {
+      return deadline;
+    }
+    return submission;
+  }
+
+  private LocalDateTime end() {
+    return start().plus(maxDuration);
+  }
+
+  long daysLeft() {
+    return LocalDateTime.now().until(end(), ChronoUnit.DAYS);
+  }
+
+  long hoursLeft() {
+    return LocalDateTime.now().until(end(), ChronoUnit.HOURS);
+  }
+
+  boolean done() {
+    return result != null;
+  }
+
+  String getResult() {
+    return result;
+  }
+}
